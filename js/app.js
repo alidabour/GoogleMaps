4378458104d5b277299ba15efe8102554c0f0f23
alidabour <alidabour0@gmail.com>
2017-07-07 15:28:01 +0200

    query

--- js/app.js
+++ js/app.js
@@ -143,6 +143,20 @@ var placeModel = function(data){
 
 
 var testModel = {
-	people:[new placeModel(locations[0])]
+	people:[{title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
+{title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
+{title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
+{title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
+{title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
+{title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}]
 };
+testModel.Query = ko.observable('');
+
+testModel.searchResults = ko.computed(function() {
+    var q = testModel.Query();
+    return testModel.people.filter(function(i) {
+      return i.title.toLowerCase().indexOf(q) >= 0;
+    });
+});
+
 ko.applyBindings(testModel);
