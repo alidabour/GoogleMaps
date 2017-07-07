0f47ba717888ef09d955cf690494c6e6c5071487
alidabour <alidabour0@gmail.com>
2017-07-07 16:43:15 +0200

    query

--- js/app.js
+++ js/app.js
@@ -143,28 +143,32 @@ function hideListings() {
 
 
 var testModel = {
-	people:[{title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
-{title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
-{title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
-{title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
-{title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
-{title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}]
+	people:[
+  {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
+  {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
+  {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
+  {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
+  {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
+  {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}],
+  showPlace: function() {
+    console.log("Hiii");
+  }
 };
 testModel.Query = ko.observable('');
 
 testModel.searchResults = ko.computed(function() {
-    var q = testModel.Query();
-    console.log(q);
-    for(var i=0; i<markers.length; i++){
-      if (markers[i].title.toLowerCase().startsWith(q.toLowerCase())) {
-        markers[i].setMap(map);
-      }else{
-        markers[i].setMap(null);
-      }
-      }
-    return testModel.people.filter(function(i) {
-      return i.title.toLowerCase().indexOf(q) >= 0;
-    });
+  var q = testModel.Query();
+  console.log(q);
+  for(var i=0; i<markers.length; i++){
+    if (markers[i].title.toLowerCase().startsWith(q.toLowerCase())) {
+      markers[i].setMap(map);
+    }else{
+      markers[i].setMap(null);
+    }
+  }
+  return testModel.people.filter(function(i) {
+    return i.title.toLowerCase().indexOf(q) >= 0;
+  });
 });
 
 ko.applyBindings(testModel);
